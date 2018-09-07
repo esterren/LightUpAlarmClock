b1e88ffb1f6488fb41acff2560913288060583ba
ESTERMANN Renato <renato.estermann@soprasteria.com>
2018-09-07 14:29:25 +0800

    Init Impl:
    - added snooze button

--- luac/main2.py
+++ luac/main2.py
@@ -1,4 +1,4 @@
-from asciimatics.widgets import Frame, Layout, Label
+from asciimatics.widgets import Frame, Layout, Label, Button
 from asciimatics.scene import Scene
 from asciimatics.screen import Screen
 from asciimatics.exceptions import ResizeScreenError, NextScene, StopApplication
@@ -6,17 +6,27 @@ from threading import Thread
 import time
 import sys
 
-class ClockView(Frame):
+class MainView(Frame):
     def __init__(self, screen: Screen):
-        super(ClockView, self).__init__(screen, screen.height, screen.width)
-        layout = Layout([1], fill_frame=True)
+        super(MainView, self).__init__(screen, screen.height, screen.width)
+        layout_top = Layout([1], fill_frame=True)
         self._screen = screen
-        self.add_layout(layout)
+        self.add_layout(layout_top)
         self._date_label = Label(label="")
-        self._time_label = Label(label="00:00:00")
-        layout.add_widget(self._date_label)
-        layout.add_widget(self._time_label)
+        self._time_label = Label(label="")
+        layout_top.add_widget(self._date_label)
+        layout_top.add_widget(self._time_label)
+
+        layout_bottom = Layout([1])
+        self.add_layout(layout_bottom)
+        layout_bottom.add_widget(Button("Snooze", self._snooze),0)
+
         self.fix()
+
+
+    def _snooze(self):
+        self.palette["background"] = (Screen.COLOUR_BLUE, Screen.A_NORMAL, Screen.COLOUR_WHITE)
+        self._screen.force_update()
 
     def tick(self):
         self._date_label.text = time.strftime("%d.%m.%y")
@@ -25,7 +35,7 @@ class ClockView(Frame):
 
 
 class Clock(Thread):
-    def __init__(self, view: ClockView, interval=0.5):
+    def __init__(self, view: MainView, interval=0.5):
         Thread.__init__(self)
         self.view = view
         self.setDaemon(True)
@@ -38,21 +48,23 @@ class Clock(Thread):
             time.sleep(self.interval)
 
 def demo(screen:Screen):
-    clock_view = ClockView(screen)
+    clock_view = MainView(screen)
     ct = Clock(view=clock_view)
     ct.start()
     scene = Scene([clock_view], -1, name="Main")
     screen.play([scene], stop_on_resize=True)
 
 if __name__ == '__main__':
-    #try:
-    #screen = Screen.open()
-    Screen.wrapper(demo, catch_interrupt=False)
-    #try:
-    #    demo(screen)
-    #finally:
-    #    Screen.close()
-    #    sys.exit(0)
-    #except ResizeScreenError as e:
-    #Screen.close()
+    screen = Screen.open()
+    #Screen.wrapper(demo, catch_interrupt=False)
+    restore = True
+    try:
+        try:
+            demo(screen)
+        except ResizeScreenError:
+            restore = False
+            raise
+    finally:
+        screen.close(restore)
+
     sys.exit(0)
